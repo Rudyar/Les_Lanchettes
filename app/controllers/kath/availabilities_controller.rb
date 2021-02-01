a9b9b3ccf2cd7a9307dd906cee4518d0477a2467
Rudyar <arthur.rudy.code@gmail.com>
2021-02-01 15:25:05 +0100

    edit and destroy availability done

--- app/controllers/kath/availabilities_controller.rb
+++ app/controllers/kath/availabilities_controller.rb
@@ -21,36 +21,36 @@ class Kath::AvailabilitiesController < ApplicationController
     end
   end
   
-  # def edit
-  #   @availability = availability.find(params[:id])
-  # end
+  def edit
+    @availability = Availability.find(params[:id])
+  end
   
-  # def update
-  #   @availability = availability.find(params[:id])
+  def update
+    @availability = Availability.find(params[:id])
     
-  #   if @availability.update(availability_params)
-  #     flash[:success] = "Période modifiée"
-  #     redirect_to kath_root_path
+    if @availability.update(availability_params)
+      flash[:success] = "Période modifiée"
+      redirect_to kath_root_path
 
-  #   else
-  #     flash[:danger] = "Impossible de modifier la période"
-  #     render :new
+    else
+      flash[:danger] = "Impossible de modifier la période"
+      render :new
 
-  #   end
-  # end
+    end
+  end
 
-  # def destroy
-  #   @availability = availability.find(params[:id])
-  #   if @availability.destroy
-  #     flash[:info] = "Période supprimée"
-  #     redirect_to kath_root_path
-  #   else
-  #     flash[:danger] = "Impossible de supprimer cette période"
-  #     render :edit
-  #   end
+  def destroy
+    @availability = Availability.find(params[:id])
+    if @availability.destroy
+      flash[:info] = "Période supprimée"
+      redirect_to kath_root_path
+    else
+      flash[:danger] = "Impossible de supprimer cette période"
+      render :edit
+    end
 
       
-  # end
+  end
 
   private
 
